0378e7611e28a6a5c24b6897a9b27134207145c5
Caio Biegai <engs-caioferreira@camporeal.edu.br>
2024-02-29 18:57:59 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import receitas from './receitas.json';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import React, { useEffect, useState } from 'react';
 
-function App(){
-  const [nutri, setNutri] = useState([]);
+class App extends Component {
+  state = {
+    receitas: []
+  };
 
-  useEffect( () => {
-        function carregaDados(){
-          let url = './receitas.JSON';
+  componentDidMount() {
+    this.setState({ receitas: receitas });
+  }
 
-          fetch(url)
-          .then((r) => r.json())
-          .then((json) => {
-            setNutri(json);
-          })
-        }
-        carregaDados();
-  },[]);
+  render() {
+    const { receitas } = this.state;
 
-  return(
-    <div>
-      <header>
-        <strong>Receita de Gelo</strong>
-      </header>
+    return (
+      <div>
+        <header className="header">
+          <div style={{ textAlign: 'left', paddingLeft: '15px' }}>
+            <h1>❄ Receitas de Gelo🧊 <span style={{ float: 'right' }} >🏂</span></h1>
+          </div>
+        </header>
 
-      {nutri.map((item) => {
-        return(
-          <article key={item.id}>
-            aaa
-          </article>
-        );
-      })}
-    </div>
-  );
+
+        <section className="container m-5">
+          <div className="receita-grid">
+            {receitas.map(receita => (
+              <div className="receita-quadro" key={receita.id}>
+                <div className="receitas3">
+                  <h2> Nº: {receita.id} {receita.title}</h2>
+                  <h4>{receita.ingredientes}</h4>
+                  <h6>{receita.modoPreparo}</h6>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
+
 export default App;
